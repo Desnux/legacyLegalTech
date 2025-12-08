33ed5e4314bb34076af800a75db725485a48c402
Desnux <nikoipinza1@gmail.com>
2025-12-08 15:12:39 -0300

    preparando archivos para empezar con los cambios

--- legal-tech-backend-develop/docker/entrypoint.sh
+++ legal-tech-backend-develop/docker/entrypoint.sh
@@ -2,24 +2,13 @@
 
 set -e
 
-# Si no viene PORT (por ejemplo, corriendo local), usa 8100 por defecto
-PORT=${PORT:-8100}
+echo "$DB_HOST:$DB_PORT"
 
-echo "App port: $PORT"
-echo "DB: ${DB_HOST:-<no DB_HOST>}:${DB_PORT:-<no DB_PORT>}"
+until pg_isready -h "$DB_HOST" -p "$DB_PORT"; do
+  echo "Waiting for PostgreSQL..."
+  sleep 5
+done
 
-# Si hay DB_HOST y DB_PORT, espera a que PostgreSQL esté listo
-if [[ -n "$DB_HOST" && -n "$DB_PORT" ]]; then
-  echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
-  until pg_isready -h "$DB_HOST" -p "$DB_PORT"; do
-    echo "Waiting for PostgreSQL..."
-    sleep 5
-  done
-else
-  echo "DB_HOST o DB_PORT no definidos, saltando espera de PostgreSQL."
-fi
-
-# Migraciones opcionales
 if [[ "${MIGRATION_ENABLED}" == "true" ]]; then
   echo "Running migrations"
   alembic upgrade head
@@ -33,10 +22,7 @@ fi
 
 exec gunicorn main:app \
     -k uvicorn.workers.UvicornWorker \
-    --workers 1 \
-    --threads 1 \
+    --workers 4 \
+    --threads 2 \
     --timeout 180 \
-    --max-requests 100 \
-    --max-requests-jitter 20 \
-    --bind 0.0.0.0:"$PORT"
-
+    --bind 0.0.0.0:8100
