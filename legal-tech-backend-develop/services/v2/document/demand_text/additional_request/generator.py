9f260bd21ed8f25c28a0599398896351e821b1a5
Desnux <nikoipinza1@gmail.com>
2025-12-17 13:41:23 -0300

    Modificar 3otrosi 5

--- legal-tech-backend-develop/services/v2/document/demand_text/additional_request/generator.py
+++ legal-tech-backend-develop/services/v2/document/demand_text/additional_request/generator.py
@@ -200,8 +200,7 @@ class DemandTextAdditionalRequestGenerator(BaseGenerator):
                 </template>
                 <specific-documents-example>
                 1. Pagaré singularizado en lo principal de esta presentación, que funda la presente ejecución, con sus respectivas hojas de prolongación.
-                2....
-                Es deber del Usuario Acompañar y nombrar el resto de documentos.
+                2. Es deber del Usuario Acompañar y nombrar el resto de documentos.
                 
                 """
             case JudicialCollectionLegalRequest.INDICATE_EMAILS:
