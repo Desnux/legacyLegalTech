bdd2dfd3fe1f36226fdb453dd9ee8f9cf8c5fe0d
Desnux <nikoipinza1@gmail.com>
2025-12-18 13:42:39 -0300

    Modificando prompt bienes

--- legal-tech-backend-develop/services/v2/document/demand_text/additional_request/generator.py
+++ legal-tech-backend-develop/services/v2/document/demand_text/additional_request/generator.py
@@ -238,7 +238,7 @@ class DemandTextAdditionalRequestGenerator(BaseGenerator):
                     base_instruction = (
                         "Redacta el OTROSÍ refiriéndote únicamente a bienes del aval. "
                         "No menciones ejecutado ni terceros y tampoco agregues la frase OtroSI."
-                        "Usa el siguiente texto ROGAMOS A US. tener presente que señalamos para la traba del embargo todos los bienes del "
+                        "Usa el siguiente texto textual, no lo modifiques ROGAMOS A US. tener presente que señalamos para la traba del embargo todos los bienes del "
                         "ejecutado que estime suficientes el Ministro de Fe encargado de la diligencia, pudiendo "
                         "embargarlos a mera petición verbal del ejecutante."
                     )
@@ -246,7 +246,7 @@ class DemandTextAdditionalRequestGenerator(BaseGenerator):
                     base_instruction = (
                         "Redacta el OTROSÍ refiriéndote a los bienes del ejecutado y aval. "
                         "No menciones a terceros y tampoco agregues la frase OtroSI."
-                        "Usa el siguiente texto ROGAMOS A US. tener presente que señalamos para la traba del embargo todos los bienes del "
+                        "Usa el siguiente texto, no lo modifiques ROGAMOS A US. tener presente que señalamos para la traba del embargo todos los bienes del "
                         "ejecutado que estime suficientes el Ministro de Fe encargado de la diligencia, pudiendo "
                         "embargarlos a mera petición verbal del ejecutante."
                         "Distingue claramente en la redacción qué bienes corresponden a cada uno, "
